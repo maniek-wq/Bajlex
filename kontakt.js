454a769ce3f344e560a017f8c10183ef75e0006d
unknown <jakwasi01@gmail.com>
2023-04-08 18:49:23 +0200

    usunieto stare pliki

--- kontakt.js
+++
@@ -1,6 +0,0 @@
-let mymap = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-  maxZoom: 18,
-}).addTo(mymap);
-const marker = L.marker([51.5, -0.09]).addTo(mymap);
